873e8899ba1b2f85066339332c1e3c391658ba2f
Sahra Mohamad <sahrasparkle2@gmail.com>
2023-05-04 18:25:45 -0700

    added finished code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // === Creating the Questions
 // 1. Create a questions variable that has an array of questions
 // 2. In each index put an object that has a question:string for its key:value pair
@@ -9,6 +8,27 @@
 // Questions Array, each question is an object
 // The object has 1 question and an answer array that has an object with a correct property
 
+const questions = [
+    {
+        question: "What is Bruce Wayne's other identity?",
+        answers: [
+            {text: "Batman", correct: true},
+            {text: "Superman", correct: false},
+            {text: "Spiderman", correct: false},
+            {text: "Aquaman", correct: false},
+        ]
+    },
+    {
+        question: "What is Clark Kent's other identity?",
+        answers: [
+            {text: "Batman", correct: false},
+            {text: "Superman", correct: true},
+            {text: "Spiderman", correct: false},
+            {text: "Aquaman", correct: false},
+        ]
+    },
+    
+];
 
 
 // === Selecting the elements / Setting Variables
@@ -17,7 +37,12 @@
 // 8. Query selector for the nextButton div, save this in a nextButton variable
 // 9. Create an index and score variable with the values of 0
 
+const questionText = document.querySelector("#questions");
+const answerButtons = document.querySelector("#answer-buttons");
+const nextButton = document.querySelector("#next-btn");
 
+let index = 0;
+let score = 0;
 
 // === Create startQuiz Function
 // 10. Have it reset the index and score to 0
@@ -25,7 +50,14 @@
 // 12. Call on the showQuestion function
 // 0. Call on start quiz
 
+function startQuiz(){
+    index = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
 
+startQuiz();
 
 // === Create the showQuestion function
 // 13. Create a currentQuestion variable set to the index var in the questions array
@@ -35,6 +67,24 @@
 // 17. add an event listener to the button for "click", selectAnswer
 // 18. add an if statement for answer.correct then button.dataset.correct = answer.correct
 
+function showQuestion() {
+    clearQuestions();
+    let currentQuestion = questions[index];
+    questionText.innerHTML = currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    })
+
+
+}
 
 
 // === Create Select Answer function with parameter e
@@ -46,13 +96,38 @@
 // 24. set button.disabled = true
 // 25. set nextButton.style.display = "block"
 
+function selectAnswer(e) {
+    let selected = e.target;
+    let isCorrect = selected.dataset.correct === "true";
+    if (isCorrect){
+        selected.classList.add("correct");
+        score++;
+    } else {
+        selected.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+        nextButton.style.display = "block";
+    });
+
+}
 
 
 // === Create clearQuestions function
 // 26. nextButton.getElementsByClassName.display = "none"
 // 27. while(answerButtons.firstChild) {answerButtons.removeChild(answerButtons.firstChild);}
 
-
+function clearQuestions(){
+    nextButton.style.display = "none";
+    // While there is a first child, remove the child (here, it is all the Answer1, 2, 3, etc)
+    while (answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
 
 // === Create showScore function
 // 28. Run clearQuestions
@@ -61,10 +136,33 @@
 // 31. Set nextButton.style.display = "block"
 // Shows the score after the last question and updates Next button to play again
 
+function showScore(){
+    clearQuestions();
+    questionText.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
 // === Create handleNextButton function
 // 32. Update the index by 1
 // 33. if index is less than questions.length showQuestion else showScore
 
+function handleNextButton(){
+    index++;
+    if (index < questions.length){
+        showQuestion();
+    } else{
+        showScore();
+    }
+}
 
 // === Create nextButton Event Listener on click
 // 34. if index is less than questions.length then handleNextButton else startQuiz
+
+nextButton.addEventListener("click", () => {
+    if (index < questions.length){
+        handleNextButton();
+    } else{
+        startQuiz();
+    }
+})
